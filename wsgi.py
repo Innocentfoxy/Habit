cc038bc3f4ef83913d94bac36b307633b342b4f6
Innocentfoxy <monkeydgrafy@gmail.com>
2026-02-23 23:47:34 +0530

    Refactor wsgi.py for app initialization

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
-# wsgi.py
-from app import create_app
-import os
+from app import create_app  # or however you create your app
 
-app = create_app()
+app = create_app()  # This should be at module level
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
